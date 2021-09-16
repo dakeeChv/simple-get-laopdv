5b0ea0ae57d8d71344e40779a8f14c8347330bad
Daky <dkartree@gmail.com>
2021-09-16 11:24:08 +0700

    add .env

--- src/config/db.go
+++ src/config/db.go
@@ -3,13 +3,20 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func Conn() *sql.DB {
 	// connection string
-	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", Env("host"), Env("port"), Env("user"), Env("password"), Env("dbname"))
+	psqlconn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("host"),
+		os.Getenv("port"),
+		os.Getenv("user"),
+		os.Getenv("password"),
+		os.Getenv("dbname"))
 
 	// open database
 	db, err := sql.Open("postgres", psqlconn)
